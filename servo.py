8e7b3309feccbacd867db0913c1abd3901d19c43
Joyce Xu <joycexu346@gmail.com>
2025-09-20 20:19:51 -0400

    servo turns 01

--- servo.py
+++ servo.py
@@ -1,1 +1,19 @@
-print("hello world")
+import time
+from adafruit_servokit import ServoKit
+
+# Initialize the ServoKit library with 16 channels.
+kit = ServoKit(channels=16)
+
+# Select the servo on channel 0.
+servo = kit.servo[0]
+
+# Turn the servo to 180 degrees.
+servo.angle = 180
+print("Servo turned to 180 degrees.")
+
+# Wait for 3 seconds.
+time.sleep(3)
+
+# Turn the servo to 0 degrees.
+servo.angle = 0
+print("Servo returned to 0 degrees.")
